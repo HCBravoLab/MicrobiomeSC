a849ca7c598fb4f486c1f2c74d5d847174d33dcf
Justin Wagner <jmwagner@umd.edu>
2015-02-21 10:31:14 -0800

    Fixed alphaDiversity*.sh

--- alphaDiversityMSD.sh
+++ alphaDiversityMSD.sh
@@ -1,3 +1,3 @@
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC alphaDiversity.AlphaDiversity data/msd_species_case_part1.txt data/msd_species_control_part1.txt  $1&
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC alphaDiversity.AlphaDiversity data/msd_species_case_part1_transpose.txt data/msd_species_control_part1_transpose.txt  $1&
 
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC alphaDiversity.AlphaDiversity data/msd_species_case_part2.txt data/msd_species_control_part2.txt $1
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC alphaDiversity.AlphaDiversity data/msd_species_case_part2_transpose.txt data/msd_species_control_part2_transpose.txt $1
--- alphaDiversityPGP.sh
+++ alphaDiversityPGP.sh
@@ -1,3 +1,3 @@
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC alphaDiversity.AlphaDiversity data/pgp_species_case_part1.txt data/pgp_species_control_part1.txt  $1&
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC alphaDiversity.AlphaDiversity data/pgp_species_case_part1_transpose.txt data/pgp_species_control_part1_transpose.txt  $1&
 
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC alphaDiversity.AlphaDiversity data/pgp_species_case_part2.txt data/pgp_species_control_part2.txt $1
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC alphaDiversity.AlphaDiversity data/pgp_species_case_part2_transpose.txt data/pgp_species_control_part2_transpose.txt $1

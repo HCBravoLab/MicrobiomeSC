44c41fb942fbd5ff44287da6d67041a61bb6ffe0
Justin Wagner <jmwagner@umd.edu>
2015-02-21 17:08:19 -0800

    updated paths for running scripts

--- differentialAbundanceHMP.sh
+++ differentialAbundanceHMP.sh
@@ -1,3 +1,3 @@
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC differentialAbundance.DifferentialAbundance data/hmp_species_case_part1.txt data/hmp_species_control_part1.txt  $1&
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin:lib/* util.GenRunnable ../FlexSC differentialAbundance.DifferentialAbundance data/hmp_species_case_part1.txt data/hmp_species_control_part1.txt  $1&
 
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC differentialAbundance.DifferentialAbundance data/hmp_species_case_part2.txt data/hmp_species_control_part2.txt $1
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin:lib/* util.EvaRunnable ../FlexSC differentialAbundance.DifferentialAbundance data/hmp_species_case_part2.txt data/hmp_species_control_part2.txt $1
--- oddsRatioHMP.sh
+++ oddsRatioHMP.sh
@@ -1,3 +1,3 @@
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC oddsRatio.OddsRatio data/hmp_species_case_part1.txt data/hmp_species_control_part1.txt  $1&
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin:lib/* util.GenRunnable ../FlexSC oddsRatio.OddsRatio data/hmp_species_case_part1.txt data/hmp_species_control_part1.txt  $1&
 
-java -cp bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC oddsRatio.OddsRatio data/hmp_species_case_part2.txt data/hmp_species_control_part2.txt $1
+java -cp bin/:../FlexSC/lib/*:../FlexSC/bin:lib/* util.EvaRunnable ../FlexSC oddsRatio.OddsRatio data/hmp_species_case_part2.txt data/hmp_species_control_part2.txt $1

de8494aa71bfc90c6e727c29b376e1997443bb2d
Xiao Wang <wangxiao1254@gmail.com>
2015-02-23 08:25:10 -0500

    .

---
+++ chiSquarePGP.sh
@@ -0,0 +1,3 @@
+java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC chiSquare.ChiSquare -s data/pgp_species_case_part1.txt -t data/pgp_species_control_part1.txt  $1&
+
+java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC chiSquare.ChiSquare -s data/pgp_species_case_part2.txt -t data/pgp_species_control_part2.txt $1
---
+++ oddsRatioMSD.sh
@@ -0,0 +1,3 @@
+java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC oddsRatio.OddsRatio data/pgp_species_case_part1.txt data/pgp_species_control_part1.txt  $1&
+
+java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC oddsRatio.OddsRatio data/pgp_species_case_part2.txt data/pgp_species_control_part2.txt $1

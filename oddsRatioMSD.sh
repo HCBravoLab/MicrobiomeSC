195370c7f910ff1b5f1564a47461bf0d6d15a392
Justin Wagner <jmwagner@umd.edu>
2015-02-23 14:24:08 -0800

    oddsratioPGP -> MSD

--- oddsRatioMSD.sh
+++ oddsRatioMSD.sh
@@ -1,3 +1,3 @@
-java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC oddsRatio.OddsRatio data/pgp_species_case_part1_nozeros.txt data/pgp_species_control_part1_nozeros.txt  $1&
+java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable ../FlexSC oddsRatio.OddsRatio data/msd_species_case_part1_nozeros.txt data/msd_species_control_part1_nozeros.txt  $1&
 
-java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC oddsRatio.OddsRatio data/pgp_species_case_part2_nozeros.txt data/pgp_species_control_part2_nozeros.txt $1
+java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable ../FlexSC oddsRatio.OddsRatio data/msd_species_case_part2_nozeros.txt data/msd_species_control_part2_nozeros.txt $1

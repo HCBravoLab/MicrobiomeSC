220f984377e392ed47165de8c5fb7fe59d64ca72
Justin Wagner <jmwagner@umd.edu>
2015-07-15 11:12:28 -0700

    cleaning scripts

--- scripts/chiSquareMSD.sh
+++
@@ -1,3 +0,0 @@
-java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable chiSquare.ChiSquare -s data/msd_species_case_part1_nozeros.txt -t data/msd_species_control_part1_nozeros.txt  $1&
-
-java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable chiSquare.ChiSquare -s data/msd_species_case_part2_nozeros.txt -t data/msd_species_control_part2_nozeros.txt $1
--- scripts/differentialAbundancePGP.sh
+++
@@ -1,3 +0,0 @@
-java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.GenRunnable differentialAbundance.DifferentialAbundance data/pgp_species_case_part1_nozeros.txt data/pgp_species_control_part1_nozeros.txt  $1&
-
-java -cp lib/*:bin/:../FlexSC/lib/*:../FlexSC/bin util.EvaRunnable differentialAbundance.DifferentialAbundance data/pgp_species_case_part2_nozeros.txt data/pgp_species_control_part2_nozeros.txt $1
